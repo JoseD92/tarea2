d559fdf1d8844507ff2e4a74bc19d0f144e141af
JoseD92 <jose_daniel_d@hotmail.com>
2015-01-20 22:48:04 -0430

    agregado programa para probar el codigo

---
+++ tarea2Try.py
@@ -0,0 +1,19 @@
+from datetime import timedelta, datetime, time
+from decimal import Decimal
+from tarea2 import tarifa, monto
+
+print('Ingrese fecha en formato: YYYY MM DD HH mm')
+print('Ingrese fecha de inicio:')
+s = input()
+s = s.split(' ')
+t1 = datetime(int(s[0]), int(s[1]), int(s[2]), int(s[3]), int(s[4]), 0, 0)
+print('Ingrese fecha de final:')
+s = input()
+s = s.split(' ')
+t2 = datetime(int(s[0]), int(s[1]), int(s[2]), int(s[3]), int(s[4]), 0, 0)
+print('Ingrese un monto para la tarifa diurna:')
+s = input()
+print('Ingrese un monto para la tarifa nocturna:')
+s2 = input()
+m = monto(t1,t2,tarifa(s,s2))
+print("El monto a pagar es: "+str(m))
